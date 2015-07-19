1eb4823c3dba0754b13a798fa6eb4fd28607ecc2
Ben Halverson <benhalverson33@gmail.com>
2015-07-19 00:07:34 -0700

    basic data

--- mainCtrl.js
+++ mainCtrl.js
@@ -2,7 +2,26 @@
 angular
 .module('mymedic')
 .controller('mainCtrl', function($scope) {
+  console.log('main ctrl loaded');
+  $scope.data = [{
+    title: 'Brain Health',
+    'percentage': 33
+  },
+  {
+    title: 'Physical Health',
+    'percentage': 33
+  },
+  {
+    title: 'Emotional Health',
+    'percentage': 33
+  }];
 
-  
-
+  $scope.tabledata = [{
+    text: 'hello',
+    body: 'hello 2'
+  },
+  {
+    text: 'hello',
+    body: 'hello 2'
+  }];
 });
